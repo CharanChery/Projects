25768874dbf022f3da41cd5d729dbc8acd979e60
CharanChery <95143627+CharanChery@users.noreply.github.com>
2024-09-28 07:24:02 +0000

    js

--- Auction Bidding Platform/public/home.js
+++ Auction Bidding Platform/public/home.js
@@ -1,12 +1,12 @@
 try {
   document.getElementById("Signup").addEventListener("click", () => {
-    const newpage = "./signup.html";
+    const newpage = "../signup.html";
     let fullurl = newpage //+ "?username=" + encodeURIComponent(urlusername);
     window.location.href = fullurl;
   });
 
   document.getElementById("Login").addEventListener("click", () => {
-    const newpage = "./login.html";
+    const newpage = "../login.html";
     let fullurl = newpage //+ "?username=" + encodeURIComponent(urlusername);
     window.location.href = fullurl;
   });
